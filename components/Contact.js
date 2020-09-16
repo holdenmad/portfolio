302b216485efa322e9c02cd16001791e124c3689
holdenmad <holden.madagame@gmail.com>
2020-09-16 14:30:58 +0200

    Adding pictures and routes

---
+++ components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './layout.module.css';
+
+
+const Contact = () => {
+  return (
+    <div className={styles.socialmedia}>
+      <div>Email </div>
+      <div>Github </div>
+      <div>LinkedIn </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ components/layout.js
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Contact from './Contact'
+
+const name = 'Holden Madagame';
+export const siteTitle = 'Junior Developer';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <>
+          {home ? (
+            <>
+              <img
+                src='/bright-headshot-dev-square copy.jpg'
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+              <Contact/>
+            </>
+          ) : (
+            <>
+              <Link href='/'>
+                <a>
+                  <img
+                    src='/bright-headshot-dev-square copy.jpg'
+                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                    alt={name}
+                  />
+                </a>
+              </Link>
+              <h2 className={utilStyles.headingLg}>
+                <Link href='/'>
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </>
+          )}
+        </>
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href='/'>
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}

a623306c915862bd0027789691cd48e76e9b51d5
Holden <holden.madagame@gmail.com>
2020-12-12 17:55:07 +0100

    Add Navigation to Portoflio and Blog

---
+++ components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./Navigation.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const Navigation = () => {
+  return (
+    <div className={styles.socialmedia}>
+      <div className={utilStyles.navLink}>
+        <Link href="/portfolio">Portfolio</Link>
+      </div>
+      <div className={utilStyles.navLink}>
+        <Link href="/blog">Blog</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;

1a346330362f4e91ec7210910ceab742495d4ac3
Holden <holden.madagame@gmail.com>
2020-12-14 15:01:32 +0100

    Style custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,19 @@
 // 404.js
-import Link from 'next/link'
+import Link from "next/link";
+import blogStyles from "../styles/blog.module.css";
+import utilStyles from "../styles/utils.module.css";
+import homeStyles from "../styles/Home.module.css";
 
 export default function FourOhFour() {
-  return <>
-    <h1>404 - Page Not Found</h1>
-    <Link href="/">
-      <a>
-        Go back home
-      </a>
-    </Link>
-  </>
+  return (
+    <div className={homeStyles.fourohfourBG}>
+      <h1 className={homeStyles.fourohfour}>404</h1>
+      <h2>Page Not Found</h2>
+      <div className={blogStyles.backToBlog}>
+        <Link href="/">
+          <a className={utilStyles.link}>← Back to home</a>
+        </Link>
+      </div>
+    </div>
+  );
 }

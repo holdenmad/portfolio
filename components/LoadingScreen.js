69389a10242cdece3f640bcec7a3dbee87c5a3e5
Holden <holden.madagame@gmail.com>
2020-12-22 19:25:42 +0100

    Style LoadingScreen with GSAP animation

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import styles from "./LoadingScreen.module.css";
 
 const LoadingScreen = () => {
+  let loadingAnimation = () => {
+    gsap.to("#loadingIcon", { duration: 4, rotation: 360 });
+  };
+
+  let loadingRef = useRef(null);
+
+  useEffect(() => {
+    loadingAnimation();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.loading}>LOADING...</div>
+      <div
+        id="loadingIcon"
+        ref={loadingRef}
+        className={styles.loadingIcon}
+      ></div>
     </div>
   );
 };
